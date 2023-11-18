08ef7258d839682fe9ed71474ee619fddd65423a
Sujana-M83 <124856014+Sujana-M83@users.noreply.github.com>
2023-11-18 12:30:55 +0530

    DAY5

---
+++ day5/errorboundarysrc/components/TestOne.js
@@ -0,0 +1,11 @@
+import React,{ Component } from "react";
+export default class TestOne extends Component{
+    render()
+    {
+        return(
+            <div>
+                <h1>Component one</h1>
+            </div>
+        );
+    }
+}
